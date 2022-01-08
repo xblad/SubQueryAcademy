2ea3fdc33d0226bf02450a93927a2379827af0cf
Daniil Bladyko <124-daniil.bladyko@users.noreply.127.0.0.1>
2022-01-08 21:51:39 +0100

    create record instead of get attemp

--- SecondModule/src/mappings/mappingHandlers.ts
+++ SecondModule/src/mappings/mappingHandlers.ts
@@ -5,7 +5,7 @@ import {Balance} from "@polkadot/types/interfaces";
 export async function handleEvent(event: SubstrateEvent): Promise<void> {
     const {event: {data: [account, balance]}} = event;
     //Retrieve the record by its ID
-    const record = await Account.get(event.extrinsic.block.block.header.hash.toString());
+    const record = new Account(event.extrinsic.block.block.header.hash.toString());
     record.account = account.toString();
     //Big integer type Balance of a transfer event
     record.balance = (balance as Balance).toBigInt();
